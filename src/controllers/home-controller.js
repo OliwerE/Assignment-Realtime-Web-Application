498c79859c966a9347198fd9e66b1f3bdf6632fc
Oliwer <oe222ez@student.lnu.se>
2021-02-16 22:24:10 +0100

    Application request issues from gitlab when a user is requesting the issues page

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -5,6 +5,8 @@
  * @version 1.0.0
  */
 
+import nodemon from "nodemon"
+
 /**
  * 
  */
--- src/controllers/issues-controller.js
+++ src/controllers/issues-controller.js
@@ -5,13 +5,43 @@
  * @version 1.0.0
  */
 
+import fetch from 'node-fetch'
+
 /**
  * 
  */
 export class IssuesController {
-  index (req, res, next) {
+fetchIssues(url) {
+  console.log('Starts fetch')
+  /*
+  await fetch(url, {
+    method: 'get',
+    headers: {
+    }
+  }).then(response => {
+    console.log(response)
+    // return response.text()
+  }).catch(err => {
+    console.log(err)
+    throw new Error('An error has occurred (getScraper)')
+  })
+  */
+}
 
-    // Get issues from gitlab here!
+  async index (req, res, next) { // fix this. then use fetchIssues instead
+    await fetch('https://gitlab.lnu.se/api/v4/projects/12822/issues', {
+      method: 'get',
+      headers: {
+        'PRIVATE-TOKEN': process.env.GITLAB_TOKEN
+      }
+    }).then(res => res.json()).then(json => {
+      console.log(json)
+    }).catch(err => {
+      console.log(err)
+      throw new Error('An error has occurred (getScraper)')
+    })
+
+    // console.log(issues)
 
     res.render('issues/index')
   }
