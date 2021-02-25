0874e1264f078bb96431e548cb8cc9f4a71facd2
Oliwer <oe222ez@student.lnu.se>
2021-02-25 20:39:23 +0100

    Added try/catch in webhook controller

--- src/controllers/webhook-controller.js
+++ src/controllers/webhook-controller.js
@@ -17,10 +17,16 @@ export class WebhookController {
    * @param {Function} next - Next function.
    */
   authorize (req, res, next) {
-    if (req.headers['x-gitlab-token'] !== process.env.GITLAB_HOOK_SECRET) {
-      res.status(403).send('Forbidden')
-    } else {
-      next()
+    try {
+      if (req.headers['x-gitlab-token'] !== process.env.GITLAB_HOOK_SECRET) {
+        res.status(403).send('Forbidden')
+      } else {
+        next()
+      }
+    } catch (err) {
+      const error = new Error('Internal Server Error')
+      error.status = 500
+      next(error)
     }
   }
 
@@ -32,17 +38,23 @@ export class WebhookController {
    * @param {Function} next - Next function.
    */
   index (req, res, next) {
-    req.body = {
-      avatar: req.body.user.avatar_url,
-      title: req.body.object_attributes.title,
-      desc: req.body.object_attributes.description,
-      state: req.body.object_attributes.state,
-      action: req.body.object_attributes.action,
-      id: req.body.object_attributes.id,
-      temp: req.body.object_attributes,
-      done: false
+    try {
+      req.body = {
+        avatar: req.body.user.avatar_url,
+        title: req.body.object_attributes.title,
+        desc: req.body.object_attributes.description,
+        state: req.body.object_attributes.state,
+        action: req.body.object_attributes.action,
+        id: req.body.object_attributes.id,
+        temp: req.body.object_attributes,
+        done: false
+      }
+      next()
+    } catch (err) {
+      const error = new Error('Internal Server Error')
+      error.status = 500
+      next(error)
     }
-    next()
   }
 
   /**
@@ -53,15 +65,21 @@ export class WebhookController {
    * @param {Function} next - Next function.
    */
   postFromWebhook (req, res, next) {
-    res.io.emit('issue', { // sends data to clients using websocket
-      avatar: req.body.avatar,
-      title: req.body.title, // Sends title of issue to all clients (when modified/created or moved open-closed)
-      desc: req.body.desc,
-      status: req.body.state, // opened or closed
-      action: req.body.action,
-      id: req.body.id,
-      iid: req.body.temp.iid
-    })
-    res.status(200).send('Post confirmed')
+    try {
+      res.io.emit('issue', { // sends data to clients using websocket
+        avatar: req.body.avatar,
+        title: req.body.title, // Sends title of issue to all clients (when modified/created or moved open-closed)
+        desc: req.body.desc,
+        status: req.body.state, // opened or closed
+        action: req.body.action,
+        id: req.body.id,
+        iid: req.body.temp.iid
+      })
+      res.status(200).send('Post confirmed')
+    } catch (err) {
+      const error = new Error('Internal Server Error')
+      error.status = 500
+      next(error)
+    }
   }
 }
