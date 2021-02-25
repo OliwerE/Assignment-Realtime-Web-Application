b4301bbd9deedd0f29e0f815a5bd00271908c42a
Oliwer <oe222ez@student.lnu.se>
2021-02-25 21:48:57 +0100

    Js client side: added comments, Changed textnode to textContent

--- public/js/index.js
+++ public/js/index.js
@@ -6,75 +6,44 @@ const issueTemplate = document.querySelector('#issueTemplate')
  * Runs websocket on the issue list page.
  */
 const issueTable = function () {
-  console.log('Issues page!') // ta bort sen!
-  const socketConnection = window.io()
+  const socketConnection = window.io() // Creates a websocket connection
 
-  socketConnection.on('issue', arg => {
-    console.log(arg)
-
-    if (arg.action === 'open') {
-      console.log('ADD ISSUE TO LIST!')
+  socketConnection.on('issue', arg => { // When an issue is recieved
+    if (arg.action === 'open') { // If new issue
       const table = document.querySelector('tbody')
 
-      // Adds table row into table
+      // Creates clone of issue template
       const newIssue = issueTemplate.content.cloneNode(true)
 
-      // add id
-      newIssue.querySelector('tr').setAttribute('id', arg.id)
+      // Add id
+      newIssue.querySelector('tr').setAttribute('id', `issue${arg.id}`)
 
-      // Add content
+      // Add title
       newIssue.querySelector('#issueName a').textContent = arg.title
 
-      if (arg.desc !== null) {
+      // Add description
+      if (arg.desc !== null) { // If an description exists
         newIssue.querySelector('#issueDesc').textContent = arg.desc
       }
 
+      // Add gravatar url
       newIssue.querySelector('#gravatar').setAttribute('src', arg.avatar)
 
-      const status = newIssue.querySelector('#issueStatus')
-      const statusTextNode = document.createTextNode(arg.status)
-      status.appendChild(statusTextNode)
+      // Add issue status
+      newIssue.querySelector('#issueStatus').textContent = arg.status
 
-      // Add url
-
-      // FIXA MED QUERYSELECTOR ALL!
+      // Add issue url
       newIssue.querySelector('#newIssueLink1').setAttribute('href', `./issues/issue/${arg.iid}`)
       newIssue.querySelector('#newIssueLink2').setAttribute('href', `./issues/issue/${arg.iid}`)
       newIssue.querySelector('#issueGravatar').setAttribute('href', `./issues/issue/${arg.iid}`)
       newIssue.querySelector('#issueStatus').setAttribute('href', `./issues/issue/${arg.iid}`)
 
-      // url.setAttribute('href', '/issues/test')
-      // url.setAttribute('href', `/issues/issue/${arg.iid}`)
-
-      console.log('iid!: ', arg.iid)
-
-      // Add id
-      newIssue.querySelector('tr').setAttribute('id', `issue${arg.id}`)
-
-      table.insertBefore(newIssue, table.firstChild)
-      console.log(table)
-    } else if (arg.action === 'reopen') {
-      console.log('REOPEN!')
-
-      // obs likt close! skapa funk ist?
+      table.insertBefore(newIssue, table.firstChild) // Adds new issue in the table
+    } else if (arg.action === 'reopen' || arg.action === 'close') { // When issue is closed or reopened
+      // Changes issue status (opened or closed)
       const string = arg.id.toString()
-      const selectTr = document.querySelector(`#issue${string} #issueStatus`)
-      selectTr.textContent = '' // Removes old status
-      const newStatusTextNode = document.createTextNode(arg.status)
-      selectTr.appendChild(newStatusTextNode)
-    } else if (arg.action === 'close') {
-      console.log('ISSUE CLOSED!')
-
-      // obs likt reopen! skapa funk ist?
-      const string = arg.id.toString()
-      const selectTr = document.querySelector(`#issue${string} #issueStatus`)
-      selectTr.textContent = '' // Removes old status
-      const newStatusTextNode = document.createTextNode(arg.status)
-      selectTr.appendChild(newStatusTextNode)
+      document.querySelector(`#issue${string} #issueStatus`).textContent = arg.status
     } else if (arg.action === 'update') {
-      // verifiera även om något har ändrats.. skickas även när öppnas/stängs issues..
-      console.log('UPDATE!')
-
       const string = arg.id.toString()
       const issueTitle = document.querySelector(`#issue${string} #issueName a`)
       const issueDesc = document.querySelector(`#issue${string} #issueDesc a`)
@@ -82,21 +51,19 @@ const issueTable = function () {
       // console.log(issueTitle.textContent)
       // console.log(issueDesc.textContent)
 
-      if (issueTitle.textContent !== arg.title) {
+      if (issueTitle.textContent !== arg.title) { // If name is changed
         issueTitle.textContent = arg.title
       }
 
-      if (issueDesc.textContent !== arg.desc) {
+      if (issueDesc.textContent !== arg.desc) { // If description is changed
         issueDesc.textContent = arg.desc
       }
     } else {
-      console.error('Something went wrong! (issue action)')
+      console.error('Something went wrong! (websocket message)')
     }
   })
 }
 
 if (issueTemplate) { // Runs websocket if the issue template exist on the current webpage.
   issueTable()
-} else {
-  console.log('not issues page!') // ta bort sen!
 }
